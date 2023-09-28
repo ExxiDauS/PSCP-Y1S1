0d324400ca0bd98505246999f956c5854d565e45
Kittipit <kpinthawornrak@gmail.com>
2023-09-29 02:42:24 +0700

    edit donut

--- Week 2/donut.py
+++ Week 2/donut.py
@@ -1,25 +1,23 @@
-'''le donut'''
-
-def donut(price, promo, extra, need):
-
-    '''le donut'''
-
-    count = 0
-    pay = 0
-
-    multi = need // (promo + extra)
-    pay += price * (promo * multi)
-    count += multi * (promo + extra)
-
-    if need > 0:
-        left = need - count
-        if left >= promo:
-            left = promo
-            count += promo + extra
+"""Ejudge"""
+def main():
+    """Donut"""
+    price = int(input())
+    buy = int(input())
+    free = int(input())
+    minwant = int(input())
+    donut = 0
+    pro = buy+free
+    buypro = minwant // pro
+    if minwant == 0:
+        print("0 0")
+    if minwant > 0:
+        donut = pro * buypro
+        left = minwant - donut
+        if left > buy:
+            left = buy
+        if left >= buy:
+            donut += pro
         else:
-            count += left
-        pay += price * left
-
-    return "%d %d" % (pay, count)
-
-print(donut(int(input()), int(input()), int(input()), int(input())))
+            donut += left
+        print("%d %d" %((price*buypro*buy)+(left*price), donut))
+main()
